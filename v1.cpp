a0ab0b36b66e1521b3afc79923591474ba8251db
setscriptable <m1ssth4v3l@gmail.com>
2026-04-02 17:13:34 +0700

    Add battery watcher utility using Windows API
    Added v1.cpp to the project, implementing a battery watcher that checks battery status using GetSystemPowerStatus. The utility prints battery level, charging state, and warns (with a beep) if battery is low while discharging. Runs in a loop, updating every 60 seconds. Project and filter files updated to include the new source.

---
+++ v1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void batterywatcher() {
+	SYSTEM_POWER_STATUS	sps;
+
+	if (GetSystemPowerStatus(&sps)) {
+		int batteryLvl = (int)sps.BatteryLifePercent;
+		int chargingState = (int)sps.ACLineStatus;
+
+		cout << "current battery: " << batteryLvl << "%" << endl;
+
+		if (chargingState == 1) {
+			cout << "charging..." << endl;
+		}
+		else {
+			cout << "discharging..." << endl;
+			if (batteryLvl <= 20) {
+				cout << "battery low!" << endl;
+				Beep(750, 500);
+			}
+		}
+	}
+	else
+	{
+		cout << "your battery died." << endl;
+	}
+}
+
+int main() {
+	while (true) {
+		batterywatcher();
+		Sleep(60000);
+	}
+	return 0;
+}
