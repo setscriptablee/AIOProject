357067b0e872a19cd415c263634c1ee04938318f
setscriptable <m1ssth4v3l@gmail.com>
2026-04-02 19:38:04 +0700

    reworked solution

--- AllInOneProject.cpp
+++ AllInOneProject.cpp
@@ -1,72 +1,103 @@
 #include <iostream>
-#include "battery.h"
+#include <Windows.h>
+
+using namespace std;
+
+void show_info()
+{
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+
+	cout << "================ system info ================" << endl;
+
+	cout << "architecture: ";
+	if (si.wProcessorArchitecture == 9) {
+		cout << "x64 (AMD64)" << endl;
+	}
+	else if (si.wProcessorArchitecture == 0) {
+		cout << "x86 (intel)" << endl;
+	}
+	else {
+		cout << "unknown" << endl;
+	}
+
+	cout << "number of processors: " << si.dwNumberOfProcessors << endl;
+
+	cout << "page size: " << si.dwPageSize << " bytes" << endl;
+
+	cout << "=============================================" << endl;
+	system("pause");
+}
 
 int main()
 {
 	bool running = true;
 	while (running) {
-		batterywatcher();
 		system("title aio toolkit - made by brutalizedshrimpo");
 		int choice = -1;
-		std::cout << "1. internal wipeout\n";
-		std::cout << "2. external wipeout\n";
-		std::cout << "3. system checkup\n";
-		std::cout << "4. 'reset' network\n";
-		std::cout << "0. exit\n";
-		std::cout << "enter your choice: ";
-		std::cin >> choice;
+		cout << "1. internal wipeout\n";
+		cout << "2. external wipeout\n";
+		cout << "3. system checkup\n";
+		cout << "4. 'reset' network\n";
+		cout << "0. exit\n";
+		cout << "enter your choice: ";
+		cin >> choice;
 
 		if (choice == 1) {
 			system("color 0A");
-			std::cout << "deleting temporary files...\n";
+			cout << "deleting temporary files...\n";
 			system("del /f /s /q %temp%\\*.*");
-			std::cout << "deleted temporary files.\n";
-			std::cout << "deleting prefetch files...\n";
+			cout << "deleted temporary files.\n";
+			cout << "deleting prefetch files...\n";
 			system("del /f /s /q %systemroot%\\prefetch\\*.*");
-			std::cout << "deleted prefetch files.\n";
+			cout << "deleted prefetch files.\n";
 			system("pause");
 		}
 		else if (choice == 2) {
-			std::cout << "deleting old windows update files...\n";
+			cout << "deleting old windows update files...\n";
 			system("Dism /Online /Cleanup-Image /StartComponentCleanup");
-			std::cout << "deleted old windows update files.\n";
-			std::cout << "cleaning browser cache...\n";
+			cout << "deleted old windows update files.\n";
+			cout << "cleaning browser cache...\n";
 			if (system("if exist \"%localappdata%\\Google\\Chrome\" (exit 0) else (exit 1)") == 0) {
-				std::cout << "found google chrome.\n";
+				cout << "found google chrome.\n";
 				system("cd /d \"%localappdata%\\Google\\Chrome\\User Data\\Default\" && del /f /s /q \"Cache\\*.*\" && del /f /s /q \"Code Cache\\*.*\" && del /f /s /q \"GPUCache\\*.*\"");
 			}
 			else {
-				std::cout << "no supported browsers found.\n";
+				cout << "no supported browsers found.\n";
 			}
 			if (system("if exist \"%localappdata%\\Microsoft\\Edge\" (exit 0) else (exit 1)") == 0) {
-				std::cout << "found microsoft edge.\n";
+				cout << "found microsoft edge.\n";
 				system("cd /d \"%localappdata%\\Microsoft\\Edge\\User Data\\Default\" && del /f /s /q \"Cache\\*.*\" && del /f /s /q \"Code Cache\\*.*\" && del /f /s /q \"GPUCache\\*.*\"");
 			}
 			else {
-				std::cout << "no supported browsers found.\n";
+				cout << "no supported browsers found.\n";
 			}
-			std::cout << "cleaned chrome/edge cache.\n";
+			cout << "cleaned chrome/edge cache.\n";
 			system("pause");
 		}
 		else if (choice == 3) {
 			system("color 0B");
-			std::cout << "running system checkup...\n";
+			cout << "running system checkup...\n";
 			system("sfc /scannow");
-			std::cout << "system checkup complete.\n";
+			cout << "system checkup complete.\n";
 			system("findstr /c:'[SR]' %windir%\\Logs\\CBS\\CBS.log > %userprofile%\\Desktop\\sfclogs.txt");
-			std::cout << "saved sfc logs to desktop.\n";
+			cout << "saved sfc logs to desktop.\n";
 			system("pause");
 		}
 		else if (choice == 4) {
 			system("color 0C");
-			std::cout << "resetting network...\n";
+			cout << "resetting network...\n";
 			system("netsh winsock reset");
-			std::cout << "network reset complete.\n";
-			std::cout << "restart your computer.\n";
+			cout << "network reset complete.\n";
+			cout << "restart your computer.\n";
 			system("pause");
 		}
+		else if (choice == 9) {
+			system("cls");
+			show_info();
+		}
 		else if (choice == 0) {
-			std::cout << "exiting program...\n";
+			cout << "exiting program...\n";
 			running = false;
 		}
 		else {
